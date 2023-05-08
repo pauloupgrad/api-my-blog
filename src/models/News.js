cd4bdec530128ca80f0ddf6a61e32be17ac0b609
pauloupgrad <pauloupgrad@gmail.com>
2023-05-08 06:54:51 -0300

    feat: criado a Schema de News

---
+++ src/models/News.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+const { Schema } = mongoose
+
+const NewsSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    banner: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    likes: {
+        type: Array,
+        required: true,
+    },
+    comments: {
+        type: Array,
+        required: true,
+    },
+})
+
+const News = mongoose.model("News", NewsSchema)
+
+export default News
